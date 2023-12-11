b209d430df9c56b2a215ebab03038383c291f02b
Дмитрий Матохин <argizol@mail.ru>
2023-12-11 23:03:03 +0300

    123

--- NetProgramm/EF/Program.cs
+++ NetProgramm/EF/Program.cs
@@ -4,8 +4,8 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var optionsBuilder = new DbContextOptionsBuilder<ChatContext>()
-                                     .UseSqlServer("Server=.;Database=Chat;Trusted_Connection=True")
-                                     .UseLazyLoadingProxies();
+        //var optionsBuilder = new DbContextOptionsBuilder<ChatContext>()
+        //                             .UseSqlServer("Server=.;Database=Chat;Trusted_Connection=True")
+        //                             .UseLazyLoadingProxies();
     }
 }
