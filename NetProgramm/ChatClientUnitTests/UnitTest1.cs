45b66ac0ec79a10115c506cdbf3d5d517974c3f4
Дмитрий Матохин <argizol@mail.ru>
2023-12-18 19:54:29 +0300

    Тесты для клиента

--- NetProgramm/ChatClientUnitTests/UnitTest1.cs
+++ NetProgramm/ChatClientUnitTests/UnitTest1.cs
@@ -1,34 +1,70 @@
-using Lesson6.Services;
 using Moq;
+using System.Net;
+using System.Net.Sockets;
 using UnitTests.Abstracts;
+using UnitTests.Models;
 using UnitTests.Services;
 
 namespace ChatClientUnitTests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public async Task ClientListener_ReceivesMessage_CallsConfirm()
         {
-            
+            // Arrange
+            var name = "TestClient";
+            var address = "127.0.0.1";
+            var port = 1234;
+
+            var messageSourceMock = new Mock<IMessageSource>();
+            var udpClientMock = new Mock<UdpClient>();
+
+            var client = new Client(name, address, port)
+            {
+                _messageSouce = messageSourceMock.Object,
+                udpClientClient = udpClientMock.Object
+            };
+
+            var remoteEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.1"), 5678);
+            var netMessage = new NetMessage { NickNameFrom = "Sender", Text = "Hello", Command = Command.Message };
+
+            messageSourceMock.Setup(m => m.Receive(ref remoteEndPoint)).Returns(netMessage);
+            messageSourceMock.Setup(m => m.SendAsync(It.IsAny<NetMessage>(), It.IsAny<IPEndPoint>())).Returns(Task.CompletedTask);
+
+            // Act
+            await client.ClientListener();
+
+            // Assert
+            messageSourceMock.Verify(m => m.Receive(ref remoteEndPoint), Times.Once);
+            messageSourceMock.Verify(m => m.SendAsync(It.IsAny<NetMessage>(), remoteEndPoint), Times.Once);
         }
 
         [Test]
-        public void TestCreate()
+        public void Register_CallsMessageSourceSendAsync()
         {
-            var client = new Client("Vasya", "172.0.0.1", 12345);
+            // Arrange
+            var name = "TestClient";
+            var address = "127.0.0.1";
+            var port = 1234;
 
-            Assert.That(client, Is.EqualTo(new Client("Vasya", "172.0.0.1", 12345)));
+            var messageSourceMock = new Mock<IMessageSource>();
+            var udpClientMock = new Mock<UdpClient>();
+
+            var client = new Client(name, address, port)
+            {
+                _messageSouce = messageSourceMock.Object,
+                udpClientClient = udpClientMock.Object
+            };
+
+            var remoteEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.1"), 5678);
+
+            // Act
+            client.Register(remoteEndPoint);
+
+            // Assert
+            messageSourceMock.Verify(m => m.SendAsync(It.IsAny<NetMessage>(), remoteEndPoint), Times.Once);
         }
 
-        [Test]
-        public void TestListener()
-        {
-            var netmsgsorce = new UdpMessageSouce();
-            var server = new Server(netmsgsorce);
-            var client = new Client("Vasya", "172.0.0.1", 12345);
-            server.Start();
-            
-        }
     }
 }
