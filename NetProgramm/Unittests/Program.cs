38d27dadc03de7efba34065e6ebaa2b784dbe9c5
Дмитрий Матохин <argizol@mail.ru>
2023-12-18 20:04:34 +0300

    123

---
+++ NetProgramm/Unittests/Program.cs
@@ -0,0 +1,17 @@
+﻿using UnitTests.Services;
+
+namespace UnitTests
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+           // var client = new Client("Vasya", "172.0.0.1", 12345);
+            var netmsgsorce = new UdpMessageSouce();
+            var server = new Server(netmsgsorce);
+            await server.Start();
+            //await client.Start();
+
+        }
+    }
+}
